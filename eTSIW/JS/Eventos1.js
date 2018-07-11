cd0690a71237d032e5cf0b1137aaec0f84092d5b
Matilde-Valente <34509096+Matilde-Valente@users.noreply.github.com>
2018-07-11 23:12:43 +0100

    Comentários e eventos completados

--- eTSIW/JS/Eventos1.js
+++ eTSIW/JS/Eventos1.js
@@ -1,5 +1,12 @@
 let eventos = [];
 let existe = false;
+let detalhesEvento;
+let catalogoEventos;
+let adicionar;
+let botao;
+let arrayComentarios = [];
+let substringID;
+let url;
 
 //CLASSE EVENTO
 class Evento {
@@ -100,6 +107,9 @@ if (localStorage.getItem("eventos") == null) {
     localStorage.setItem("eventos", JSON.stringify(eventos));
 }
 
+//Vai buscar a key utilizadores e guarda no array utilizadores
+utilizadores = JSON.parse(localStorage.getItem("utilizadores"));
+
 window.onload = function () {
     console.log(localStorage.getItem("eventos"))
 
@@ -108,67 +118,93 @@ window.onload = function () {
     }
     console.log(eventos)
 
-    let caminho = window.location.pathname.split('/')
-    console.log(caminho)
-    if (caminho[8] == "Eventos.html") {
+    adicionar = document.getElementById("modalEventos")
+    detalhesEvento = document.getElementById("detalhes")
+    catalogoEventos = document.getElementById("card")
+
+    if (catalogoEventos != null) {
         criarCard();
     }
-    let caminho2 = caminho[8].split('#')
-    if (caminho2[0] == "vEventos.html") {
+    if (detalhesEvento != null) {
         carregarEventos();
+        arrayComentarios = eventos[substringID - 1]._comentario
+        mostrarComentarios();
     }
 
-    let adicionar = document.getElementById("modalEventos")
+    if (adicionar != null) {
+        adicionar.addEventListener("submit", adicionarEvento);
+    }
+}
 
-    adicionar.addEventListener("submit", function () {
-        console.log("ola")
+function adicionarEvento() {
+    let strErro = "";
+    let nomeEvento = document.getElementById("modalNomeEvento")
+    let descricao = document.getElementById("modalDescricao")
+    let data = document.getElementById("modalData")
+    let hora = document.getElementById("modalHora")
+    let sala = document.getElementById("modalSala")
+    let responsavel = document.getElementById("modalResponsavel")
+    let parceria = document.getElementById("modalParceria")
+    let imagem = document.getElementById("modalImagemEvento")
+    url = imagem.value
 
-        let strErro = "";
-        let nomeEvento = document.getElementById("modalNomeEvento")
-        let descricao = document.getElementById("modalDescricao")
-        let data = document.getElementById("modalData")
-        let hora = document.getElementById("modalHora")
-        let sala = document.getElementById("modalSala")
-        let responsavel = document.getElementById("modalResponsavel")
-        let parceria = document.getElementById("modalParceria")
-        let imagem = document.getElementById("modalImagemEvento")
+    //Data Atual
+    let dataAtual = new Date();
+    let dd = dataAtual.getDate();
+    let mm = dataAtual.getMonth() + 1; //Janeiro é 0
+    let aaaa = dataAtual.getFullYear();
 
-        for (let i = 0; i < eventos.length; i++) {
-            if (nomeEvento.value == eventos[i]._nome) {
-                strErro = "Já existe um evento com esse nome!\nPor favor insira outro nome"
-                existe = true
-                event.preventDefault();
-            }
-            else if (sala.value == eventos[i]._sala) {
-                strErro += "\nA sala já está ocupada"
-                existe = true
-                event.preventDefault();
-            }
-            else {
-                existe = false
-            }
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    dataAtual = aaaa + '-' + mm + '-' + dd;
+
+    //Verificações
+    if (data.value <= dataAtual) {
+        strErro += "\nA data escolhida é inválida! Por favor escolha outra data."
+    }
+    for (let i = 0; i < eventos.length; i++) {
+        if (nomeEvento.value == eventos[i]._nome) {
+            strErro += "\nJá existe um evento com esse nome! Por favor insira outro nome."
+            existe = true
+            event.preventDefault();
         }
-        console.log("passou o primeiro if");
-        if (strErro == "") {
-            if (existe == false) {
-                let novoEvento = new Evento(nomeEvento.value, descricao.value, data.value, hora.value, sala.value, responsavel.value, parceria.value, imagem.value, "", "")
-                eventos.push(novoEvento);
-                alert("Registo criado com sucesso")
-
-                localStorage.setItem("eventos", JSON.stringify(eventos));
-                console.log(localStorage);
-            }
-            console.log("criou o str error");
+        if (sala.value == eventos[i]._sala) {
+            strErro += "\nA sala já está ocupada! Por favor escolha outra sala"
+            existe = true
+            event.preventDefault();
+        }
+        if (imagem.value == "") {
+            url = "https://pbs.twimg.com/profile_images/792011747397865473/2r6wseRk_400x400.jpg"
+            existe = false
         }
         else {
-            alert(strErro)
+            existe = false
         }
-    });
+    }
+
+    console.log("passou o primeiro if");
+    if (strErro == "") {
+        if (existe == false) {
+            let novoEvento = new Evento(nomeEvento.value, descricao.value, data.value, hora.value, sala.value, responsavel.value, parceria.value, url, arrayComentarios, "")
+            eventos.push(novoEvento);
+            alert("Registo criado com sucesso")
+
+            localStorage.setItem("eventos", JSON.stringify(eventos));
+            console.log(localStorage);
+        }
+        console.log("criou o str error");
+    }
+    else {
+        alert(strErro)
+    }
 }
 
 //Adiciona dados ao card
 function criarCard() {
-
     let strCard = ""
 
     for (var i = 0; i < eventos.length; i++) {
@@ -178,7 +214,7 @@ function criarCard() {
         // Cria o card
         strCard += `<div class="col-sm-4">
         <div class="card" onclick="abrirEventos(${eventos[i]._id})" style="width: 18rem;">
-            <img class="card-img-top" src="${eventos[i]._imagem}" alt="Card image cap">
+            <img class="card-img-top" style="width: 286px; heigth: 286px" src="${eventos[i]._imagem}" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">${eventos[i]._nome}</h5>
                 <p class="card-text">${eventos[i]._descricao}</p>`
@@ -189,7 +225,7 @@ function criarCard() {
             strCard += `</div>`
         }
     }
-    let catalogoEventos = document.getElementById("card")
+    catalogoEventos = document.getElementById("card")
     catalogoEventos.innerHTML = strCard
 }
 
@@ -204,7 +240,7 @@ let eventoCarregado = []
 function carregarEventos() {
     let id = window.location.hash
 
-    let substringID = id.substring(1)
+    substringID = id.substring(1)
     console.log(substringID)
 
     for (let i = 0; i < eventos.length; i++) {
@@ -216,22 +252,75 @@ function carregarEventos() {
     console.log(eventoCarregado)
     let strDetalhes = ""
 
-    for (let i = 0; i < eventoCarregado.length; i++) {
-        strDetalhes += `<div class="col-12">
-        <img class="img-responsive w-100 h-100" src="${eventoCarregado[i]._imagem}">
-    </div>
-    <div class="col-12">
-        <h1 class="display-3 text-center m-3">${eventoCarregado[i]._nome}</h1>
-        <p class="text-center" style="font-size: 11pt" id=""><b>&ensp; Data &emsp; &emsp; Hora &emsp; &nbsp; Sala </b></p>
-        <p class="lead text-center" style="font-size: 12pt" id="eDetalhes">${eventoCarregado[i]._data} &nbsp; <b> | </b> &nbsp; ${eventoCarregado[i]._hora} &nbsp;<b> | </b> &nbsp; ${eventoCarregado[i]._sala}</p>
-        <p class="lead">${eventoCarregado[i]._descricao}</p>`
 
-        strDetalhes += `</div>`
-        console.log("entrou")
-    }
-    console.log(eventoCarregado)
+    strDetalhes += `<div class="col-1"></div>
+        <div class="col-10"><div class="jumbotron mt-5">
+        <img class="img-responsive w-100 h-100" src="${eventoCarregado[0]._imagem}">
+    
+        <div><h1 class="display-3 text-center m-3">${eventoCarregado[0]._nome}</h1></div>
+        <div><center><table class='w-25' style='text-align:center'>
+        <thead>
+            <tr><th>Data</th><th>Hora</th><th>Sala</th></tr>
+        </thead>
+        <tbody>
+            <tr><td>${eventoCarregado[0]._data}</td><td>${eventoCarregado[0]._hora}</td><td>${eventoCarregado[0]._sala}</td></tr>
+        </tbody>
+        </table ></center></div>
+        <p class="lead">${eventoCarregado[0]._descricao}</p>
+        <div><textarea id="comentarioTextArea" class="w-100" cols="150" rows="4"></textarea></div>
+        <div style="text-align:right"><button id="comentarioBotao" type="button" onclick="adicionarComentario()" value="Enviar">Enviar</button></div></div>`
 
-    // console.log(strDetalhes)
-    let detalhesEvento = document.getElementById("detalhes")
+    strDetalhes += `</div>`
+
+    detalhesEvento = document.getElementById("detalhes")
     detalhesEvento.innerHTML += strDetalhes
 }
+
+function adicionarComentario() {
+    botao = document.getElementById("comentarioBotao")
+    console.log(botao)
+
+    let comentarioTextArea = document.getElementById("comentarioTextArea")
+    let comentarioNovo = comentarioTextArea.value
+    let arrayLogado = JSON.parse(localStorage.getItem("logado"));
+    let nomeLogado = arrayLogado[0]
+
+    console.log(arrayLogado)
+
+    arrayComentarios.push({ nomeLogado, comentarioNovo });
+    console.log(arrayComentarios)
+
+    console.log(eventos)
+    eventos[substringID - 1]._comentario = arrayComentarios;
+    localStorage.setItem("eventos", JSON.stringify(eventos));
+    window.location.reload()
+
+    comentarioTextArea.value = ""
+}
+
+function mostrarComentarios() {
+    let strComentarios = ""
+    let img;
+
+    strComentarios += `<div class="col-1"></div>
+    <div class="col-10"><div class="jumbotron">`
+
+    for (let i = 0; i < arrayComentarios.length; i++) {
+        for (let j = 0; j < utilizadores.length; j++) {
+            console.log("ola")
+            if (arrayComentarios[i].nomeLogado == utilizadores[j]._nome) {
+                img = utilizadores[j]._foto
+            }
+
+        }
+        strComentarios += `<div><h4><img  class="rounded border border-secondary m-2" style="width: 40px; height: 40px" src="${img}"></img>${arrayComentarios[i].nomeLogado}</h4></div><div class="mb-2">${arrayComentarios[i].comentarioNovo}</div><hr>`
+        console.log("entrei")
+    }
+    strComentarios += `</div></div>`
+    console.log(img)
+    console.log(arrayComentarios.length)
+
+    let comentarios = document.getElementById("comentarios")
+    comentarios.innerHTML += strComentarios
+}
+
--- eTSIW/JS/Inicio.js
+++ eTSIW/JS/Inicio.js
@@ -1,7 +1,10 @@
 //VARIAVEIS GLOBAIS
-let utilizadores;
+let utilizadores = [];
+let docentes = [];
+let parcerias = [];
 let registado;
-let logado = []
+let logado = [];
+let link;
 
 //CLASSE UTILIZADOR
 class Utilizador {
@@ -58,16 +61,138 @@ class Utilizador {
     }
 }
 
+//CLASSE DOCENTE
+class Docente {
+    constructor(nome, email, pass, foto, formacao, unidadesCurriculares, shortCV) {
+        this._nome = nome;
+        this._email = email;
+        this._pass = pass;
+        this._foto = foto;
+        this._formacao = formacao;
+        this._unidadesCurriculares = unidadesCurriculares;
+        this._shortCV = shortCV;
+    }
+
+    get nome() {
+        return this._nome
+    }
+    set nome(novoNome) {
+        this._nome = novoNome
+    }
+
+    get email() {
+        return this._email
+    }
+    set email(novoEmail) {
+        this._email = novoEmail
+    }
+
+    get pass() {
+        return this._pass
+    }
+    set pass(novaPass) {
+        this._pass = novaPass
+    }
+
+    get foto() {
+        return this._foto
+    }
+    set foto(novaFoto) {
+        this._foto = novaFoto;
+    }
+
+    get formacao() {
+        return this._formacao;
+    }
+    set formacao(novaFormacao) {
+        this._formacao = novaFormacao;
+    }
+
+    get unidadesCurriculares() {
+        return this._unidadesCurriculares;
+    }
+    set unidadesCurriculares(novaUnidadesCurriculares) {
+        this._unidadesCurriculares = novaUnidadesCurriculares;
+    }
+
+    get shortCV() {
+        return this._shortCV;
+    }
+    set shortCV(novoShortCV) {
+        this._shortCV = novoShortCV;
+    }
+}
+
+//CLASSE PARCERIA
+class Parceria {
+    constructor(instituicao, localizacao, link) {
+        this._codigo = Parceria.ultimoId() + 1
+        this._instituicao = instituicao;
+        this._localizacao = localizacao;
+        this._link = link;
+    }
+
+    get instituicao() {
+        return this._instituicao;
+    }
+    set instituicao(novaInstituicao) {
+        this._instituicao = novaInstituicao
+    }
+
+    get localizacao() {
+        return this._localizacao;
+    }
+    set localizacao(novaLocalizacao) {
+        this._localizacao = novaLocalizacao;
+    }
+
+    get link() {
+        return this._instituicao;
+    }
+    set link(novoLink) {
+        this._link = novoLink;
+    }
+
+    static ultimoId() {
+        let ultimoId = 0
+        if (parcerias.length != 0) {
+            ultimoId = parcerias[parcerias.length - 1]._id
+        }
+        return ultimoId
+    }
+}
+
 //Criar admin caso key utilizadores esteja vazia
 if (localStorage.getItem("utilizadores") == null) {
     utilizadores.push(new Utilizador("admin", "admin@esmad.ipp.pt", "admin", " ", "admin"));
     localStorage.setItem("utilizadores", JSON.stringify(utilizadores));
 }
 
+//Criar docentes na local storage
+if (localStorage.getItem("docentes") == null) {
+    docentes.push(new Docente("Mário Pinto", "mariopinto@esmad.ipp.pt", "12345", "../Imagens/logo_2.png", "Doutoramento", "Algoritmia e Estrutura de Dados, Bases de Dados", "Ciências de Computadores, UPT"));
+    docentes.push(new Docente("Marta Fernandes", "marta@esmad.ipp.pt", "12345", " ", "Mestrado", "Fundamentos de Design, Design Gráfico", "CV Desconhecido"));
+    docentes.push(new Docente("Filomena Soares", "filomenasoares@esmad.ipp.pt", "12345", " ", "Doutoramento", "Matemática I, Matemática II", "Análise de Fundamentos, Matemática, UPT"));
+    docentes.push(new Docente("André Baltazar", "andrebaltazar@esmad.ipp.pt", "12345", " ", "Doutoramento", "Sistemas Computacionais", "Ciência e Tecnologia das Artes, UCP"));
+    docentes.push(new Docente("Ricardo Queirós", "ricardoqueiros@esmad.ipp.pt", "12345", " ", "Doutoramento", "Programação Orientada a Objetos", "Ciências de Computadores, FCUP"));
+    docentes.push(new Docente("Teresa Terroso", "teresaterroso@esmad.ipp.pt", "12345", " ", "Doutoramento", "Animação Gráfica, Sistemas Gráficos", "Engenharia Informática, FEUP"));
+    docentes.push(new Docente("João Azevedo", "joaoazevedo@esmad.ipp.pt", "12345", " ", "Formação Desconhecida", "Conceção e Produção Multimédia", "CV Desconhecido"));
+    docentes.push(new Docente("Luís Mourão", "luismourao@esmad.ipp.pt", "12345", " ", "Doutoramento", "Fisica Aplicada à Programação", "CV Desconhecido"));
+    docentes.push(new Docente("Jorge Lima", "jorgelima@esmad.ipp.pt", "12345", " ", "Licenciatura", "Análise e Modelação de Sistemas", "Matemática e Ciências da Computação, Desconhecido"));
+    docentes.push(new Docente("Carlos Portela", "carlosportela@esmad.ipp.pt", "12345", " ", "Doutoramento", "Programação Web I", "Tecnologias e Sistemas de Informação, UM"));
+    docentes.push(new Docente("Rui Rodrigues", "ruirodrigues@esmad.ipp.pt", "12345", " ", "Formação Desconhecida", "Modelação e Ambientes 3D", "CV Desconhecido"))
+    localStorage.setItem("docentes", JSON.stringify(docentes));
+    console.log("Docentes adicionados à Local Storage")
+}
+
 window.onload = function () {
     let registar = document.getElementById("registar")
     let login = document.getElementById("login")
     let logout = document.getElementById("logout")
+    let eventosBotao = document.getElementById("eventos")
+    let testemunhos = document.getElementById("testemunhos")
+    let cDocentes = document.getElementById("consultarDocente")
+    let configuracoes = document.getElementById("configuracoes")
     let submitLogin = document.getElementById("submitLogin");
     let submitRegistar = document.getElementById("submitRegistar");
 
@@ -78,26 +203,44 @@ window.onload = function () {
     //verificar se está feito login, e se sim, info de utilizador
     logado = JSON.parse(localStorage.getItem("logado"));
 
-
     //Esconder botões de login e registo consoante login validado ou nao
     if (logado == null) {
-
         registar.style.display = 'block';
         login.style.display = 'block';
         logout.style.display = 'none';
-
+        eventosBotao.style.display = 'block';
+        testemunhos.style.display = 'block';
+        configuracoes.style.display = 'none';
+        cDocentes.style.display = 'block';
     }
     else {
         registar.style.display = 'none';
         login.style.display = 'none';
         logout.style.display = 'block';
         console.log("alguem ta logado")
+
+        for (let i = 0; i < utilizadores.length; i++) {
+            if (utilizadores[i]._estatuto == "admin") {
+                eventosBotao.style.display = 'block';
+                testemunhos.style.display = 'block';
+                configuracoes.style.display = 'block';
+                cDocentes.style.display = 'block';
+            }
+        }
+
+        for (let i = 0; i < docentes.length; i++) {
+            if (docentes[i]._estatuto == "docentes") {
+                eventosBotao.style.display = 'block';
+                testemunhos.style.display = 'block';
+                configuracoes.style.display = 'none';
+                cDocentes.style.display = 'block';
+            }
+        }
     }
 
-    logout.addEventListener("click", function(event) {
+    logout.addEventListener("click", function (event) {
         localStorage.removeItem("logado");
         location.reload();
-    
     });
 
     registo.addEventListener("submit", function (event) {
@@ -106,11 +249,11 @@ window.onload = function () {
         let rPass = document.getElementById("modalPass")
         let rConfPass = document.getElementById("modalConfPass")
         let rFoto = document.getElementById("modalFoto")
-
+        link = rFoto.value;
 
         let strErro = ""
 
-        //Verificar se as passes sao iguais
+        //Verificar se as passes são iguais
         if (rPass.value != rConfPass.value) {
             strErro += "As passwords tem de ser iguais"
             rPass.focus()
@@ -119,7 +262,7 @@ window.onload = function () {
             console.log(utilizadores)
             //ler array de utilizadores
             for (let i = 0; i < utilizadores.length; i++) {
-                //ver se ja existe email usado
+                //ver se já existe email usado
                 if (utilizadores[i]._email == rEmail.value) {
                     strErro = "Email já existente"
                     registado = true;
@@ -128,7 +271,7 @@ window.onload = function () {
                     break;
                 }
                 else {
-                    //ver se nome ja existe
+                    //ver se nome já existe
                     if (utilizadores[i]._nome == rNome.value) {
                         strErro = "Utilizador já existente"
                         registado = true;
@@ -141,12 +284,16 @@ window.onload = function () {
                         registado = false
                     }
                 }
+                if (rFoto.value == "") {
+                    link = "https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_960_720.png"
+                    registado = false
+                }
             }
         }
 
         if (strErro == "") {
             if (registado == false) {
-                let novoUtilizador = new Utilizador(rNome.value, rEmail.value, rPass.value, rFoto.value, "estudante")
+                let novoUtilizador = new Utilizador(rNome.value, rEmail.value, rPass.value, link, "estudante")
                 utilizadores.push(novoUtilizador);
                 alert("Registo criado com sucesso")
                 console.log(utilizadores);
@@ -154,7 +301,6 @@ window.onload = function () {
                 localStorage.setItem("utilizadores", JSON.stringify(utilizadores));
                 console.log(localStorage);
             }
-
         } else {
             alert(strErro)
             registado = false;
@@ -188,8 +334,55 @@ window.onload = function () {
             alert("Email ou palavra passe incorretos!")
             event.preventDefault();
         }
-
-
     })
+    criarCardDocente();
+}
+
+function criarCardDocente() {
+    if (docentes <= 0) {
+        docentes = JSON.parse(localStorage.getItem("docentes"));
+    }
+
+    let strCardDocente = ""
+
+    console.log("estou dentro da funcao")
+    console.log(docentes)
+
+    for (let i = 0; i < docentes.length; i++) {
+        if (i % 3 == 0) {
+            strCardDocente += `<div class="row">`
+        }
+        console.log("dentro do for")
+        // Cria o card
+        strCardDocente += `<div class="col-sm-4">
+            <div class="card" style="width: 18rem" onClick="abrirCardDocente(${i})" data-toggle="modal" data-target="#modalProf">
+                <img class="card-img-top" src="${docentes[i]._imagem}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${docentes[i]._nome}</h5>`
+
+        strCardDocente += `</div></div></div>`
+
+        if (i % 3 == 2) {
+            strCardDocente += `</div>`
+        }
+    }
+    let catalogoDocentes = document.getElementById("cardDocente")
+    catalogoDocentes.innerHTML = strCardDocente
+}
+
+function abrirCardDocente(n) {
+    let nomeProf = document.getElementById("nomeProf")
+    let fotoProf = document.getElementById("fotoProf")
+    let emailProf = document.getElementById("emailProf")
+    let formacaoProf = document.getElementById("formacaoProf")
+    let unidadeCurricularProf = document.getElementById("unidadeCurricularProf")
+    let cvProf = document.getElementById("cvProf")
+
+    nomeProf.innerHTML = docentes[n]._nome;
+    fotoProf.src = docentes[n]._foto;
+    emailProf.innerHTML = docentes[n]._email;
+    formacaoProf.innerHTML = docentes[n]._formacao;
+    unidadeCurricularProf.innerHTML = docentes[n]._unidadesCurriculares;
+    cvProf.innerHTML = docentes[n]._shortCV;
 }
 
